ad7a70fa660fb19dee8540471f5d58fb64740626
Alejandro <aledurax@gmail.com>
2020-10-22 15:04:26 -0400

    fix: Error in deploy

--- app/events/mutations/createEvent.ts
+++ app/events/mutations/createEvent.ts
@@ -4,7 +4,6 @@ import db, { EventCreateArgs } from "db"
 type CreateEventInput = Pick<EventCreateArgs, "data">
 export default async function createEvent({ data }: CreateEventInput, ctx: Ctx) {
   ctx.session.authorize()
-
   const event = await db.event.create({ data })
 
   return event
--- app/events/pages/events/[eventId]/edit.tsx
+++ app/events/pages/events/[eventId]/edit.tsx
@@ -10,7 +10,7 @@ import EventForm from "app/events/components/EventForm"
 
 export const EditEvent = () => {
   const router = useRouter()
-  const eventId = useParam("eventId", "number")
+  const eventId = useParam("eventId", "string")
   const [event, { mutate }] = useQuery(getEvent, { where: { id: eventId } })
   const [updateEventMutation] = useMutation(updateEvent)
 
