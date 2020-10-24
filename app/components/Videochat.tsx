a4514a902f0416aae4d9e5f7af98230d45dbd2b6
aashnamahajan <mahajan.aashna@gmail.com>
2020-10-24 15:18:45 -0400

    added local files

--- app/components/Videochat.tsx
+++ app/components/Videochat.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Jitsi from 'react-jitsi'
 
 interface Props {
 
@@ -7,11 +8,15 @@ interface Props {
  * This is the JitsiVideochat component
  * DEVELOP:
  */
+
+const roomName = 'my-super-secret-meeting-123e4567-e89b-12d3-a456-426655440000'
+const userFullName = 'Aashna Mahajan'
+ 
 const JitsiVideochat: React.FC<Props> = (props) => {
     return (
-        <div>
-            Videochat
-        </div>
+        // <div>
+        //     <Jitsi roomName={roomName} displayName={userFullName} />
+        // </div>
     )
 }
 
