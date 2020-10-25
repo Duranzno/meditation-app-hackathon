0766af72a027c3ad5337525babb382d21e423c6a
Samuel Cook <scook@esri.com>
2020-10-24 20:50:27 -0500

    categories migration

--- app/hooks/useEvents.ts
+++ app/hooks/useEvents.ts
@@ -5,9 +5,10 @@ import db from "db"
 
 // Events are now related to the User that creates them
 // this Hook should grab the complete array of Events belonging to a user
+// TODO: debug, i don't think this is really working because of something to do with prisma fluent 
+// queries and the way that the BlitzPageComponent works...
 export const useOwnedEvents = () => {
   const session = useSession()
-  console.log(session.userId);
   const events = useQuery(getEvents, { where: { userId: session.userId } })
   return events;
 }
