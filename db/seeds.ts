cc4969493c4b626df72a1be0d889b88f59864848
SantiagoSalazarPavajeau <santiago.s.pavajeau@gmail.com>
2020-10-25 00:21:23 -0400

    add seeding data/ faker not working

--- db/seeds.ts
+++ db/seeds.ts
@@ -1,4 +1,5 @@
-// import db from "./index"
+import db from "./index"
+import faker from "faker";
 
 /*
  * This seed function is executed when you run `blitz db seed`.
@@ -8,9 +9,39 @@
  * realistic data.
  */
 const seed = async () => {
-  // for (let i = 0; i < 5; i++) {
-  //   await db.project.create({ data: { name: "Project " + i } })
-  // }
+  const mindfulness = await db.category.create({data: {name: "Mindfulness"}})
+  const spiritual = await db.category.create({data: {name: "Spiritual"}})
+  const focused = await db.category.create({data: {name: "Focused"}})
+  const movement = await db.category.create({data: {name: "Movement"}})
+  const mantra = await db.category.create({data: {name: "Mantra"}})
+    
+  for (let i = 0; i < 10; i++) {
+    await db.event.create({
+      data: {
+        name: `${faker.date.weekday()} Meditation`, 
+        title: `Peaceful Meditation`, 
+        description: faker.random.words(), 
+        datetime: faker.date.future(), 
+        duration: Math.floor(Math.random() * (90 - 30) + 30), 
+        online: Math.random() >= 0.5, 
+        location: faker.address.city(),
+        Category: {
+          connect: {
+            id: Math.floor(Math.random() * (5 - 1) + 1),
+          },
+        },
+        User: {
+          connect: {
+            id: Math.floor(Math.random() * (5 - 1) + 1),
+          },
+        },
+      },
+    })
+  }
+  
+  
 }
 
-export default seed
+export default seed;
+
+const event =  await db.event.create({data: {name: `Meditation`, title: `editation`, description: 'Peaceful', datetime: new Date(), duration: 30, online: true, location: "LA",Category: {connect: {id: 1,},},},})
